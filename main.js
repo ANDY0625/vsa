a5bf181b5256f22fd23e15913da7de838c251196
ANDY0625 <87014291+ANDY0625@users.noreply.github.com>
2022-04-01 20:20:20 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -11,4 +11,16 @@ recognition.onresult = function(event) {
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
+    speak();
 }
+function speak(){
+    var synt=window.speechSynthesis;
+    speak_document=document.getElementById("textbox").value;
+    var utterthis=new SpeechSynthesisUtterance(speak_document);
+    synt.speak(utterthis);
+    Webcam.attach(camera);
+}
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360, height:250, image_format:'jpeg',jpeg_quality:90
+});
